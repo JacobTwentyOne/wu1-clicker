6ba4b94e20dec31b4d77624d0ff38c7bf46fdf9f
JacobTwentyOne <94972570+JacobTwentyOne@users.noreply.github.com>
2023-04-19 09:22:16 +0200

    benbitar till klickadekatter

--- js/clicker.js
+++ js/clicker.js
@@ -225,14 +225,14 @@ function createCard(upgrade) {
     } else {
         header.textContent = `${upgrade.name}, +${upgrade.clicks} per klick.`;
     }
-    cost.textContent = `Köp för ${upgrade.cost} KlickadeKatter.`;
+    cost.textContent = `Köp för ${upgrade.cost} Klickade Katter.`;
 
     card.addEventListener('click', (e) => {
         if (money >= upgrade.cost) {
             acquiredUpgrades++;
             money -= upgrade.cost;
             upgrade.cost *= 1.5;
-            cost.textContent = 'Köp för ' + upgrade.cost + ' KlickadeKatter';
+            cost.textContent = 'Köp för ' + upgrade.cost + ' Klickade Katter';
             moneyPerSecond += upgrade.amount ? upgrade.amount : 0;
             moneyPerClick += upgrade.clicks ? upgrade.clicks : 0;
             message('Grattis du har köpt en uppgradering!', 'success');
